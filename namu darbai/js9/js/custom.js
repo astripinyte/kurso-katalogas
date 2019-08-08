25dccac91317cb6f525c2e747ce8d16ceff2f596
astripinyte <astripinyte@gmail.com>
2019-08-08 15:41:40 +0300

    namu darbai- JS9

---
+++ namu darbai/js9/js/custom.js
@@ -0,0 +1,22 @@
+//let masyvas [] (elementu seka) Argumentas
+//function belekas (argumentas)
+
+//funkcijai turi buti paduotas masyvas
+function spausdintiVardusIrPavardesIsarasa(vardai){
+	
+	document.write("<ol>");
+		for(let i = 0; i < vardai.length;i++){
+			document.write("<li>"+vardai[i]+"</li>");
+		}
+	
+	
+	document.write("</ol>");
+	
+}
+
+
+
+
+
+
+
