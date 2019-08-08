f94bbb237ca8f02ec875de4407055b1edb80b154
astripinyte <astripinyte@gmail.com>
2019-08-08 15:33:26 +0300

    namu darbai- JS5

---
+++ namu darbai/js5/js/custom.js
@@ -0,0 +1,22 @@
+function arPilnametis(amzius){
+	//jeigu amzius daugiau arba lygu 18, grazinama su return true
+	if(amzius >= 18){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+//antras budas:
+function arPilnametis2(amzius){
+	//graina tiesa arba ne, true or false bus ivedus amziu
+	return amzius >=18;
+	
+}
+
+
+
+
+
+
